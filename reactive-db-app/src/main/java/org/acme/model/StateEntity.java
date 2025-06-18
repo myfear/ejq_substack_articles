62b74a649678b0e6b025740b4473ac103e0e2cf9
Eric Deandrea <eric.deandrea@gmail.com>
2025-06-18 13:43:59 -0400

    Fix

--- reactive-db-app/src/main/java/org/acme/model/StateEntity.java
+++ reactive-db-app/src/main/java/org/acme/model/StateEntity.java
@@ -2,40 +2,31 @@ package org.acme.model;
 
 import java.time.Instant;
 
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.annotations.UuidGenerator;
-import org.hibernate.type.SqlTypes;
-
-import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
 @Entity
 @Table(name = "wizard_state")
-public class StateEntity extends PanacheEntityBase {
-@Id
+public class StateEntity {
+    @Id
     @GeneratedValue
     @UuidGenerator
     public String id;
 
     @JdbcTypeCode(SqlTypes.JSON)
-    public State state; 
+    public State state;
 
-    @Column(name = "current_step")
     public int currentStep;
-
-    @Column(name = "created_at")
     public Instant createdAt;
-
-    @Column(name = "updated_at")
     public Instant updatedAt;
+    public Instant expiresAt;
 
-    @Column(name = "expires_at")
-    public Instant expiresAt; 
-    
     public StateEntity() {
         this.createdAt = Instant.now();
         this.updatedAt = Instant.now();
