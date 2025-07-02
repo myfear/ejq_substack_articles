4b370a142df7859f6bcd7bef071ace70b04ea215
Markus Eisele <markus@redhat.com>
2025-07-02 16:38:21 +0200

    direct RetrievalAugmentor

--- ai-document-assistant/src/main/java/com/ibm/developer/service/DocumentAssistant.java
+++ ai-document-assistant/src/main/java/com/ibm/developer/service/DocumentAssistant.java
@@ -4,7 +4,7 @@ import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 
-@RegisterAiService(retrievalAugmentor = RetrievalAugmentorExample.class)
+@RegisterAiService
 public interface DocumentAssistant {
     
     @SystemMessage("""
--- ai-document-assistant/src/main/java/com/ibm/developer/service/RetrievalAugmentorExample.java
+++ ai-document-assistant/src/main/java/com/ibm/developer/service/RetrievalAugmentorExample.java
@@ -1,37 +1,28 @@
 package com.ibm.developer.service;
 
-import java.util.function.Supplier;
-
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.rag.DefaultRetrievalAugmentor;
 import dev.langchain4j.rag.RetrievalAugmentor;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
 
-@ApplicationScoped
-public class RetrievalAugmentorExample implements Supplier<RetrievalAugmentor> {
 
-    private final RetrievalAugmentor augmentor;
+public class RetrievalAugmentorExample {
 
-    RetrievalAugmentorExample(EmbeddingStore store, EmbeddingModel model) {
-        // Configure the content retriever, responsible for fetching relevant content based on the user query
+    @Produces
+    @ApplicationScoped
+    public RetrievalAugmentor create(EmbeddingStore store, EmbeddingModel model) {
         var contentRetriever = EmbeddingStoreContentRetriever.builder()
-          .embeddingModel(model)
-          .embeddingStore(store)
-          .maxResults(3)
-          .build();
+                .embeddingModel(model)
+                .embeddingStore(store)
+                .maxResults(3)
+                .build();
 
-        // Create the RetrievalAugmentor that combines the retriever and a default content injector
-        augmentor = DefaultRetrievalAugmentor
-            .builder()
-            .contentRetriever(contentRetriever)
-            .build();
-    }
-
-    @Override
-    public RetrievalAugmentor get() {
-      return augmentor;
+        return DefaultRetrievalAugmentor.builder()
+                .contentRetriever(contentRetriever)
+                .build();
     }
 
 }
